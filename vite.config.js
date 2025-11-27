161ffb109666f312e651a9f50ffdf742ca4b148b
merosman91 <merghanigasimosman@gmail.com>
2025-11-27 13:23:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
       manifest: {
         name: 'شمسين - مزرعة دواجن لاحم',
         short_name: 'شمسين',
@@ -16,11 +19,22 @@ export default defineConfig({
         background_color: '#fff7e6',
         theme_color: '#f59e0b',
         icons: [
-          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { 
+            src: '/icons/icon-192.png', 
+            sizes: '192x192', 
+            type: 'image/png' 
+          },
+          { 
+            src: '/icons/icon-512.png', 
+            sizes: '512x512', 
+            type: 'image/png' 
+          }
         ]
       }
     })
   ],
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
