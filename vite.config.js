d7be5e01cd00e14d734a725f5043a4309f3e8c28
merosman91 <merghanigasimosman@gmail.com>
2025-11-27 10:02:03 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+>>> filename: vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'شمسين - مزرعة دواجن لاحم',
+        short_name: 'شمسين',
+        lang: 'ar',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fff7e6',
+        theme_color: '#f59e0b',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+  build: { outDir: 'dist' }
+})
