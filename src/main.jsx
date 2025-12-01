69e0550b09418b5aaea46b2e011561235f071caa
merosman91 <merghanigasimosman@gmail.com>
2025-12-01 18:31:07 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { FarmProvider } from './context/FarmContext.jsx'
-import './styles/index.css'
+import './src/styles/index.css'
 
 // إخفاء شاشة التحميل عند تهيئة React
 const hideLoadingScreen = () => {
